b0ee4395f7a6710eb7491fd148d9f4f5d7479212
buuug7 <youpp@126.com>
2020-02-02 18:04:40 +0800

    update

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -2,6 +2,11 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../App";
 import { ReactComponent as LogoIcon } from "../logo.svg";
+import { ReactComponent as IconGrid } from "bootstrap-icons/icons/list-ol.svg";
+import { ReactComponent as IconPeople } from "bootstrap-icons/icons/people.svg";
+import { ReactComponent as IconPerson } from "bootstrap-icons/icons/sun.svg";
+
+import { ReactComponent as IconSearch } from "bootstrap-icons/icons/search.svg";
 
 import "./header.component.scss";
 
@@ -18,14 +23,35 @@ const HeaderComponent: React.FC = () => {
           </span>
         </Link>
         <div className="app-header-right">
-          <div />
-          <ul>
-            <li>
-              <Link to={context.user ? "/me" : "/login"} className="app-header-link">
-                {context.user?.name || "登陆"}
-              </Link>
-            </li>
-          </ul>
+          <div className="app-menu">
+            <Link to={"/rank"} className="app-header-link">
+              <IconGrid style={{ fontSize: "1.5rem", marginRight: ".2rem" }} />
+              <span>排行</span>
+            </Link>
+            <Link to={"/some"} className="app-header-link">
+              <IconPeople style={{ fontSize: "1.5rem", marginRight: ".2rem" }} />
+              其他
+            </Link>
+          </div>
+          <div style={{ flex: 1 }}></div>
+          <div className="app-header-search">
+            <form>
+              <div className="input-group">
+                <div className="input-group-append">
+                  <IconSearch />
+                </div>
+                <input type="text" className="form-control search-input" />
+              </div>
+            </form>
+          </div>
+          <div className="app-menu">
+            <Link to={"/"} className="app-header-link">
+              <IconPerson style={{ fontSize: "1.5rem", marginRight: ".2rem" }} />
+            </Link>
+            <Link to={context.user ? "/me" : "/login"} className="app-header-link">
+              {context.user?.name || "登陆"}
+            </Link>
+          </div>
         </div>
       </div>
     </header>
